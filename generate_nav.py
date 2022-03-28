479a32d48917e75d8fb57b9a85cf1fa5c6a34eea
k4w1h0r53 <kamihorse@outlook.com>
2022-03-28 10:53:12 +0000

    test

--- generate_nav.py
+++ generate_nav.py
@@ -14,6 +14,6 @@ for i in contents_table:
 
 command= input("是否含有目录/目录是否和封面在同一页(y/n):")
  if command == "y":
-#     count=+1
+    count=+1
 #     contents.append("表紙 && 目次")
 #     print("添加章节: 表紙 && 目次","当前章节数:",count)
