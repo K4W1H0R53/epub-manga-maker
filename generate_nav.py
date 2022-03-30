169619dba71e2d67709dd72334305e11a12bd2ad
k4w1h0r53 <kamihorse@outlook.com>
2022-03-30 13:04:36 +0000

    初版landmark代码

--- generate_nav.py
+++ generate_nav.py
@@ -1,18 +1,39 @@
 import os
 import sys
+import re
 
 root = sys.path[0]
-contents = []
-landmark = []
+contents = []   ## 用于储存章节名
+pages = []  ## 用于储存章节开始页文件名
+landmarks = []   ## 用于储存landmark
 
-with open(root+"/contents.txt",'r',encoding='UTF-8-sig') as f:
+with open(root+"/templates/contents.txt",'r',encoding='UTF-8-sig') as f:
     contents_table= f.read().splitlines()   ## 读取contents.txt文本文件，文本文件以"$章节名|$起始页文件名"的方式储存正文目录
+    for i in contents_table:
+        a = i.split('|',2)[0]
+        b = i.split('|',2)[1]
+        contents.append(a)
+        pages.append(b)
+        if i.find("#") != -1:    ## 跳过星标行
+            print("跳过",i)
+            continue
+        if i.find("*") != -1:    ## 确认正文行
+            content = [a,b,"bodymatter"]
+            landmarks.append(content)
+            print ("生成landmarks标签:",content)
+        if i.find("cover") != -1:
+            content = [a,b,"cover"]
+            landmarks.append(content)
+            print ("生成landmarks标签:",content)
+        if i.find("目次")!= -1:
+            content = [a,b,"toc"]
+            landmarks.append(content)
+            print ("生成landmarks标签:",content)
+        if i.find("奥付")!= -1:
+            content = [a,b,"colophon"]
+            print ("生成landmarks标签:",content)
+            landmarks.append(content)
 
-for i in contents_table:
-    print(i.split('|',1)[0])
-
-# command= input("是否含有目录/目录是否和封面在同一页(y/n):")
-# if command == "y":
-#     count=+1
-#     contents.append("表紙 && 目次")
-#     print("添加章节: 表紙 && 目次","当前章节数:",count)
+print("章节表：",contents)
+print("页数表：",pages)
+print("landmark表：",landmarks)
