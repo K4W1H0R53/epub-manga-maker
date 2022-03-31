b18c287e3e4c893c5c184774dae425d43089dfc2
k4w1h0r53 <kamihorse@outlook.com>
2022-03-31 07:39:00 +0000

    test

--- generate_epub.py
+++ generate_epub.py
@@ -3,9 +3,12 @@ import pathlib
 import os
 import sys
 
-dict = pathlib.Path("./temp")
+# os.system("zip -X0 ./test.epub ./temp/mimetype")
+# os.system("zip -9 -r ./test.epub ./temp/META-INF ./temp/OEBPS")
 
-with zipfile.ZipFile("test.epub","w",zipfile.ZIP_STORED) as archive:
+dict = pathlib.Path("./temp")
+with zipfile.ZipFile("test.epub","a",zipfile.ZIP_STORED) as archive:
+    archive.writestr("mimetype", "application/epub+zip")
     for file_path in dict.rglob("*"):
         archive.write(file_path, arcname=file_path.relative_to(dict))
 
