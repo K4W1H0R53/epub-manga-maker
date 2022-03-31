dab6d77858172c8c7b56a5fa19683e63ef4c44ba
k4w1h0r53 <kamihorse@outlook.com>
2022-03-31 11:07:24 +0000

    更改生成epub路径

--- generate_epub.py
+++ generate_epub.py
@@ -8,7 +8,7 @@ import sys
 # os.system("zip -9 -r ./test.epub ./temp/META-INF ./temp/OEBPS")
 
 dict = pathlib.Path("./temp")
-with zipfile.ZipFile("test.epub","a",zipfile.ZIP_STORED) as archive:
+with zipfile.ZipFile("./books/test.epub","a",zipfile.ZIP_STORED) as archive:
     archive.writestr("mimetype", "application/epub+zip")
     for file_path in dict.rglob("*"):
         archive.write(file_path, arcname=file_path.relative_to(dict))
