f39b329e6557acf97c0ca628459a39ae825e2cb3
k4w1h0r53 <kamihorse@outlook.com>
2022-04-22 07:41:08 +0800

    test

--- epub_modules.py
+++ epub_modules.py
@@ -88,26 +88,10 @@ def gene_xhtml(title):
             img = cv2.imdecode(np.fromfile(os.path.join(path,file_name), dtype=np.uint8), 1)
             src_width = img.shape[1]
             src_height = img.shape[0]
-            page_html = '''<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html>
-            <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">  <head>
-                <meta charset="UTF-8"/>
-                <title>{3}</title>
-                <meta name="viewport" content="width={0}, height={1}" />
-                </head>  <body style="margin:0;padding:0;">
-                <div>
-                    <svg style="margin:0;padding:0;" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 {0} {1}">
-                    <image width="{0}" height="{1}" xlink:href="../Images/{2}"/>
-                    </svg>
-                </div>
-                </body>
-            </html>'''.format(src_width, src_height, file_name,title[0])
+            xhtml_content = open("./templates/blank_templates.xhtml","r",encoding="utf-8")
+            page_html = xhtml_content.format(src_width, src_height, file_name,title[0])
             f = open("./temp/OEBPS/Text/" + os.path.splitext(file_name)[0] + ".xhtml", mode="w", encoding="utf-8")
             f.write(page_html)
-            f = open("./templates/blank_templates.xhtml", mode="r", encoding="utf-8")
-            content = f.read()
-            content = content.format(title[0])
-            f = open("./temp/OEBPS/Text/blank.xhtml", mode= "w", encoding = "utf-8")
-            f.write(content)
             f.close()
             spine_file = []
             for index, name in enumerate(filename_list):
