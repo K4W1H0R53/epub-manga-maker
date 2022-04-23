93e3a66664b9daef3553a3fbb59c02f6b580658b
k4w1h0r53 <kamihorse@outlook.com>
2022-04-23 16:34:44 +0800

    test

--- gene_epub.py
+++ gene_epub.py
@@ -270,7 +270,7 @@ input("打包epub")
 import zipfile,pathlib
 dict = pathlib.Path("./temp")
 # with zipfile.ZipFile("./["+identifier+"]","["+author+"]",+title,"["+source+"]","["+uploader+"]"".epub","a",zipfile.ZIP_STORED) as archive:
-with zipfile.ZipFile("./test.epub","a",zipfile.ZIP_STORED) as archive:
+with zipfile.ZipFile("./test.epub","w",zipfile.ZIP_STORED) as archive:
     archive.writestr("mimetype", "application/epub+zip")
     for file_path in dict.rglob("*"):
         archive.write(file_path, arcname=file_path.relative_to(dict))
